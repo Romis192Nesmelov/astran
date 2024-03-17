080860dd1dfc98353876bd786d2b5f6f7f1b3b31
Romis <romis@nesmelov.com>
2024-03-17 08:42:04 +0300

    docs modals

---
+++ resources/views/blocks/document_block.blade.php
@@ -0,0 +1,6 @@
+<div class="d-flex justify-content-center align-items-center">
+    <a class="doc-href" href="{{ asset('pdfs/'.$doc.'.pdf') }}" target="_blank">
+        <img class="doc-icon" src="{{ asset('images/doc_icon.png') }}" />
+        <b>{{ $name }}</b>
+    </a>
+</div>
